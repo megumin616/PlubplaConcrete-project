de67b21bdc4c3478b6b3b27e71e1bbff734607bf
megumin61 <toncom616@gmail.com>
2024-05-22 10:26:06 +0700

    add Responsive navbar menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../Navbar/Navbar.css'
 import { Link } from 'react-router-dom'
 
+import menu from '../../assets/Images/Menu/menu.png'
+import menuX from '../../assets/Images/Menu/menuX.png'
+
 export default function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav")
+  }
+
+
   return (
     <div className='navbar'>
         <div className='navbar-logo'>
             <h1>Logo</h1>
         </div>
 
-        <div className='navbar-list-text'>
+        <div className='navbar-list-text' ref={navRef}>
             <a><Link to='/'>หน้าแรก</Link></a>
             <a><Link to='/products'>สินค้า</Link></a>
             <a><Link to='/activity'>กิจกรรม</Link></a>
@@ -17,7 +27,16 @@ export default function Navbar() {
             <a><Link to='/aboutus'>ลูกค้าไว้วางใจ</Link></a>
             <a><Link to='/contact'>ติดต่อเรา</Link></a>
             <a>ตารางน้ำหนัก</a>
+
+            <button onClick={showNavbar} className="nav-btn nav-close-btn">
+              <img src={menuX}/>
+            </button>
         </div>
+
+        <button onClick={showNavbar} className="nav-btn nav-dis-none">
+          <img src={menu}/>
+        </button>
+
     </div>
   )
 }
